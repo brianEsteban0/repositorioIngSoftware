e6c0c38ebedbe68e0bd60f4a0b8234d983c2d060
ivan.perez2001@alumnos.ubiobio.cl <ivan.perez2001@alumnos.ubiobio.cl>
2023-10-29 03:18:29 -0300

    Ter2

--- backend/src/routes/postulante.routes.js
+++ backend/src/routes/postulante.routes.js
@@ -5,13 +5,10 @@ const express = require('express');
 const authorizationMiddleware = require("../middlewares/authorization.middleware.js");
 const PostulanteController = require('../controllers/postulante.controller.js');
 const cupos = require("../middlewares/cupos.middleware.js");
-const authenticationMiddleware = require("../middlewares/authentication.middleware.js");
 const checkfecha = require("../middlewares/checkfecha.middleware.js").default;
 const crearEvaluacion = require("../middlewares/crearEvaluacion.middleware.js");
 
 const router = express.Router();
-
-router.use(authenticationMiddleware);
 
 
 router.get('/',authorizationMiddleware.isUsuario,PostulanteController.getPostulantes);

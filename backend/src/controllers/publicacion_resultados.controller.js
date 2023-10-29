bff16964471c7b2710f76918e8bbceccd54de51a
Brian Insanne <brian.cabezas2001@alumnos.ubiobio.cl>
2023-10-29 03:55:40 -0300

    modificaciones en envio de correos

--- backend/src/controllers/publicacion_resultados.controller.js
+++ backend/src/controllers/publicacion_resultados.controller.js
@@ -5,6 +5,7 @@ const Publicacion_resultados_service = require("../services/publicacion_resultad
 const { handleError } = require("../utils/errorHandler");
 const Publicacion_resultados = require('../models/publicacion_resultados.model');
 const ResultadoService = require("../services/resultado.service.js"); // Importa el servicio de resultados
+const Postulante = require("../models/postulante.model.js");
 
 // Obtiene una publicación de resultados
 async function getPublicacion_resultados(req, res) {
@@ -28,23 +29,34 @@ async function getPublicacion_resultados(req, res) {
 }
 
 // Crea una nueva publicación de resultados
-  async function createPublicacion_resultados(req, res) {
+async function createPublicacion_resultados(req, res) {
     try {
         const { body } = req;
-        const [Publicacion_resultados, error_publicacion_resultados] = await PublicacionService.createPublicacion_resultados(body);
+
+        // Verifica si existen postulantes antes de continuar
+        const postulantes = await Postulante.find();
+
+        if (postulantes.length === 0) {
+            return respondError(req, res, 400, "No hay postulantes para publicar resultados por lo tanto la publicación se cancelará");
+        }
+
+        // Continúa con la creación de la publicación de resultados
+        const [Publicacion_resultados, error_publicacion_resultados] = await Publicacion_resultados_service.createPublicacion_resultados(body);
+
         if (error_publicacion_resultados) return respondInternalError(req, res, 404, error_publicacion_resultados);
 
-      if (error_publicacion_resultados) return respondError(req, res, 400, error_publicacion_resultados);
-      if (!Publicacion_resultados) {
-        return respondError(req, res, 400, "No se entregaran los resultados");
-      }
+        if (!Publicacion_resultados) {
+            return respondError(req, res, 400, "No se entregarán resultados");
+        }
 
-      respondSuccess(req, res, 201, Publicacion_resultados);
+        respondSuccess(req, res, 201, Publicacion_resultados);
     } catch (error) {
-      handleError(error, "publicacion_resultados.controller -> createPublicacion_resultados");
-      respondError(req, res, 500, "No se creo la publicacion de resultados");
+        handleError(error, "publicacion_resultados.controller -> createPublicacion_resultados");
+        respondError(req, res, 500, "No se creó la publicación de resultados");
     }
-  }
+}
+
+
 
 // Actualiza una publicación de resultados
   async function updatePublicacion_resultados(req, res) {
@@ -79,7 +91,7 @@ async function getPublicacion_resultados(req, res) {
       const publicacion_resultados = await Publicacion_resultados.findByIdAndRemove(id);
 
       if (!publicacion_resultados) {
-        return res.status(404).json({ message: ' publicación no encontrada, intente nuevamente' });
+        return res.status(404).json({ message: ' publicación no encontrada, no se pudo eliminar intente nuevamente' });
     }
 
     return res.status(204).send();

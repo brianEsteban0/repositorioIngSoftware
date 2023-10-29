8590cee4139691915225a9d25fb2970aff5f4f7f
brianEsteban0 <brian.alarcon2001@alumnos.ubiobio.cl>
2023-10-29 04:17:34 -0300

    ivan arregla tu codigo pa que funcione

--- backend/src/middlewares/checkfecha.middleware.js
+++ backend/src/middlewares/checkfecha.middleware.js
@@ -1,23 +1,20 @@
-import { findById } from '../models/publicacion.model.js';
-
+const Publicacion = require('../models/publicacion.model.js');
 
 async function checkFechas(req, res, next) {
   const now = new Date();
 
   const publicacionId = req.body.publicacionId; // Obtén el ID de la publicación
-  const publicacion = await findById(publicacionId);
+  const publicacion = await Publicacion.findById(publicacionId);
 
   if (!publicacion) {
     return res.status(404).json({ message: 'Publicación no encontrada' });
   }
 
   if (!moment(now).isBetween(publicacion.fecha_inicio, publicacion.fecha_termino, null, '[]')) {
-    return res.status(403).json({ message: 'La accion no es posible en este momento' });
+    return res.status(403).json({ message: 'La acción no es posible en este momento' });
   }
 
   next();
 }
 
-export default {
-    checkFechas,
-} 
+module.exports =  checkFechas;
--- backend/src/routes/postulante.routes.js
+++ backend/src/routes/postulante.routes.js
@@ -3,6 +3,7 @@
 const express = require('express');
 
 const authorizationMiddleware = require("../middlewares/authorization.middleware.js");
+const authenticationMiddleware = require("../middlewares/authentication.middleware.js");
 const PostulanteController = require('../controllers/postulante.controller.js');
 const cupos = require("../middlewares/cupos.middleware.js");
 const checkfecha = require("../middlewares/checkfecha.middleware.js").default;
@@ -12,11 +13,11 @@ const router = express.Router();
 router.use(authenticationMiddleware);
 
 
-router.get('/',authorizationMiddleware.isUsuario,PostulanteController.getPostulantes);
-router.post('/',[checkfecha,cupos,authorizationMiddleware.isUsuario,crearEvaluacion],PostulanteController.createPostulantes);
-router.put('/:id',[checkfecha,authorizationMiddleware.isUsuario],PostulanteController.updatePostulantes);
-router.delete('/:id',[checkfecha,authorizationMiddleware.isUsuario],PostulanteController.deletePostulantes);
+//router.get('/',authorizationMiddleware.isUsuario,PostulanteController.getPostulantes);
+//router.post('/',[checkfecha,cupos,authorizationMiddleware.isUsuario,crearEvaluacion],PostulanteController.createPostulantes);
+//router.put('/:id',[checkfecha,authorizationMiddleware.isUsuario],PostulanteController.updatePostulantes);
+//router.delete('/:id',[checkfecha,authorizationMiddleware.isUsuario],PostulanteController.deletePostulantes);
 
 
-router.get('/postulacion/:postulacionId',PostulanteController.getPostulantesBypostulacionId);
+//router.get('/postulacion/:postulacionId',PostulanteController.getPostulantesBypostulacionId);
 module.exports = router;

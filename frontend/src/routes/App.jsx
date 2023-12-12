ee622fb15435057505d3b67db1e700d432d7651b
brianEsteban0 <brian.alarcon2001@alumnos.ubiobio.cl>
2023-12-11 21:35:49 -0300

    interactuando con la api

--- frontend/src/routes/App.jsx
+++ frontend/src/routes/App.jsx
@@ -1,7 +1,37 @@
+import { useAuth } from '../context/AuthContext';
+import { test } from '../services/auth.service';
+import React, { useEffect, useState } from 'react';
+
 function App() {
+  
+  const { user } = useAuth();
+
+  const [datos, setDatos] = useState([]);
+
+  useEffect(() => {
+    // Llama a la función test cuando el componente se monta
+    const fetchData = async () => {
+      try {
+        const response = await test();
+        // Actualiza el estado con los datos devueltos por la función test
+        setDatos(response);
+      } catch (error) {
+        console.error('Error al obtener datos:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+
   return (
     <>
       <h1>Pagina principal</h1>
+      <h1>Datos de Publicaciones:</h1>
+      <ul>
+        {datos.map((item) => (
+          <li key={item.id}>{item.titulo}</li>
+        ))}
+      </ul>
     </>
   );
 }
--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -30,12 +30,15 @@ export const logout = () => {
 
 export const test = async () => {
   try {
-    const response = await axios.get('/users');
+    const response = await axios.get('/publicaciones');
     const { status, data } = response;
+
     if (status === 200) {
-      console.log(data.data);
+      console.log(data.data); // Puedes dejar este console.log para verificar la estructura de los datos
+      return data.data; // Devuelve la respuesta directamente
     }
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    throw error; // Lanza el error nuevamente para que pueda ser manejado por el código que llama a la función
   }
 };

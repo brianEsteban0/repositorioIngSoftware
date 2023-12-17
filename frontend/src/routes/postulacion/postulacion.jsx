772da47cf639d2b20ea79d4040a02ff3c89f5f3e
mikufan82 <sebastian.gonzalez2003@alumnos.ubiobio.cl>
2023-12-17 13:05:49 -0300

    se arreglan estilos de publicacionn y postulacion

--- frontend/src/routes/postulacion/postulacion.jsx
+++ frontend/src/routes/postulacion/postulacion.jsx
@@ -43,78 +43,102 @@ function FormularioPostulacion({ publicacionId }) {
   };
 
   return (
-    <div className="container" style={{ fontFamily: 'Arial, sans-serif', padding: '20px' }}>
+    <div className="container mt-5">
       <h1>Formulario de Postulación</h1>
       <form onSubmit={handleSubmit}>
-        {/* Aquí coloca los campos del formulario según tu modelo */}
-        <label htmlFor="organizacion">Organización:</label>
-        <input
-          type="text"
-          id="organizacion"
-          value={postulanteData.Organizacion}
-          onChange={(e) => handleInputChange('Organizacion', e.target.value)}
-        />
-        {/* Aquí coloca los campos del formulario según tu modelo */}
-        <label htmlFor="descripcion">Descripcion:</label>
-        <input
-          type="text"
-          id="descripcion"
-          value={postulanteData.descripcion}
-          onChange={(e) => handleInputChange('descripcion', e.target.value)}
-        />
-        <label htmlFor="Ubicación">Ubicación:</label>
-        <input
-          type="text"
-          id="Ubicación"
-          value={postulanteData.Ubicación}
-          onChange={(e) => handleInputChange('Ubicación', e.target.value)}
-        />
-        <label htmlFor="Representante">Representante:</label>
-        <input
-          type="text"
-          id="Representante"
-          value={postulanteData.Representante}
-          onChange={(e) => handleInputChange('Representante', e.target.value)}
-        />
-        <label htmlFor="Rut_Representante">Rut_Representante:</label>
-        <input
-          type="text"
-          id="Rut_Representante"
-          value={postulanteData.Rut_Representante}
-          onChange={(e) => handleInputChange('Rut_Representante', e.target.value)}
-        />
-        <label htmlFor="Rut_Organizacion">Rut_Organizacion:</label>
-        <input
-          type="text"
-          id="Rut_Organizacion"
-          value={postulanteData.Rut_Organizacion}
-          onChange={(e) => handleInputChange('Rut_Organizacion', e.target.value)}
-        />
-        <label htmlFor="Telefono">:</label>
-        <input
-          type="text"
-          id="Telefono"
-          value={postulanteData.Telefono}
-          onChange={(e) => handleInputChange('Telefono', e.target.value)}
-        />
-        <label htmlFor="Correo">:</label>
-        <input
-          type="text"
-          id="Correo"
-          value={postulanteData.Correo}
-          onChange={(e) => handleInputChange('Correo', e.target.value)}
-        />
-        <label htmlFor="publicacionn">pub:</label>
-        <input
-          type="text"
-          id="publicacionn"
-          value={postulanteData.publicacion}
-          onChange={(e) => handleInputChange('publicacion', e.target.value)}
-        />
-        <button type="submit">Enviar Postulación</button>
+        <div className="mb-3">
+          <label htmlFor="organizacion" className="form-label">Organización:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="organizacion"
+            value={postulanteData.Organizacion}
+            onChange={(e) => handleInputChange('Organizacion', e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="descripcion" className="form-label">Descripción:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="descripcion"
+            value={postulanteData.descripcion}
+            onChange={(e) => handleInputChange('descripcion', e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Ubicación" className="form-label">Ubicación:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="Ubicación"
+            value={postulanteData.Ubicación}
+            onChange={(e) => handleInputChange('Ubicación', e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Representante" className="form-label">Representante:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="Representante"
+            value={postulanteData.Representante}
+            onChange={(e) => handleInputChange('Representante', e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Rut_Representante" className="form-label">Rut Representante:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="Rut_Representante"
+            value={postulanteData.Rut_Representante}
+            onChange={(e) => handleInputChange('Rut_Representante', e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Rut_Organizacion" className="form-label">Rut Organización:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="Rut_Organizacion"
+            value={postulanteData.Rut_Organizacion}
+            onChange={(e) => handleInputChange('Rut_Organizacion', e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Telefono" className="form-label">Teléfono:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="Telefono"
+            value={postulanteData.Telefono}
+            onChange={(e) => handleInputChange('Telefono', e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Correo" className="form-label">Correo:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="Correo"
+            value={postulanteData.Correo}
+            onChange={(e) => handleInputChange('Correo', e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="publicacion" className="form-label">Publicación:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="publicacion"
+            value={postulanteData.publicacion}
+            onChange={(e) => handleInputChange('publicacion', e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Enviar Postulación</button>
       </form>
     </div>
   );
 }
-
 export default FormularioPostulacion;
